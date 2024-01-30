b5341169e4fa850a0a305b1770d8857e6a1f0ba7
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2024-01-30 13:58:10 +0100

    adding jwt, khun security packages...

--- Moodle.API/Moodle.API/Program.cs
+++ Moodle.API/Moodle.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Moodle.BLL.Infrastructure;
 using Moodle.BLL.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using Be.Khunly.Security;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MoodleContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<JwtSecurityTokenHandler>();
+builder.Services.AddScoped<JwtManager>();
+builder.Services.AddSingleton(builder.Configuration.GetSection("Jwt").Get<JwtManager.JwtConfig>());
+
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
 
 var app = builder.Build();
 
